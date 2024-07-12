983cd79d328491c9f9fe497bb5c075db3338d798
bouchrabenkhelifa <benbouchra314@gmail.com>
2024-07-12 15:33:11 +0300

    responsive footer & responsive searchbar

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -3,7 +3,7 @@ import { FaSearch } from "react-icons/fa";
 
 const SearchBar = () => {
     return (
-        <div className="flex shadow-sm rounded-lg w-full max-w-2xl pt-24">
+        <div className="flex shadow-sm rounded-lg  max-w-2xl pt-24 md:w-full sm:w-[75%]">
             <input
                 type="text"
                 className="flex-grow h-10 px-4 text-gray-700 border border-gray-300 rounded-l-lg focus:outline-none focus:ring-2 focus:ring-[#FFC300] placeholder:text-sm placeholder:font-normal"
